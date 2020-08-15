6a9863fcecb9b7d0a13fea1adc4e8dd90ff9dae0
Mahbub Hossain Faisal <mahbubhossain249@gmail.com>
2020-08-16 01:30:43 +0600

    Closure example

--- function.js
+++ function.js
@@ -1,23 +1,45 @@
-//Immediately Invoked Function expression (IIFE)
+/*
+Closures: An inner function has always access to the variables and
+parameters of it's outer function,even after the outer function has returned.
+
+*/
+
+function retirement(retirementAge){
+	var statement = ' years reamining for retirement.';
+
+	return function (birthYear) {
+		var age = 2020 - birthYear;
+		console.log(retirementAge - age + statement);
+	}
+}
 
 
-//without passing parameters
-(function (){
+ var US = retirement(66); // second (1980) is for second anonymous function parameter
+ US(1980); //this will call the anonymous function
 
-	var score = Math.random()*10;
-	console.log(score >= 5);
-} ) (); //IIFE means we don't have to call this function . It will call automatically itself .
+ console.log(US); // this will show the whole anonymous function . Means the US variable has stored the who anonymous function.
 
+ //Another example of closures
 
+ function question(job){
 
-//with parameters passing
+ 	return function(name){
+ 		if(job === 'Developer'){
+ 			console.log('Hi ' + name + ' , You are a Developer');
+ 		} else if(job === 'Designer'){
+ 			console.log('Hi ' + name + ' , You are a Designer');
+ 		} else {
 
-(function (el){
+ 			console.log('Hi ' + name + ' , Who are you?');
+ 		}
+ 		}
+ 	} 
+ 	/*job parameter is declared earlier and even the question function already returned something
+ 	though we used the job value later in the anonymous function.this is an example of closure */
+ 
+ var Faisal = question('Designer');
+ Faisal('Faisal');
 
-	var score = Math.random()*10;
-	console.log(score >= 5);
-	console.log(el);
-})('good luck!');   //IIFE means we don't have to call this function . It will call automatically itself .
+ var Asif = question('Developer')('Asif');
 
-
-//IIFE is also used for data privacy.
+ var Raihan = question()('Raihan');
