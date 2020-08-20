111c12e2e523736a3958f90d0deafb7cf184a750
Mahbub Hossain Faisal <mahbubhossain249@gmail.com>
2020-08-21 01:17:51 +0600

    findIndex and find - array method ES6

--- ES6/script.js
+++ ES6/script.js
@@ -1,26 +1,17 @@
-//Loops
+//Two new array methos findIndex and find
 
-//in ES6 we cannot use break or continue statements in forEach loop and also in map
+//in ES5
+var ages = [16, 14, 15, 21, 10];
 
-var boxes = document.querySelectorAll(".box");
+var fullAge = ages.map(function (el) {
+  return el >= 18;
+});
 
-var ES5Boxes = Array.prototype.slice.call(boxes);
+console.log(fullAge.indexOf(true)); //to find the index whose value returns true;
+console.log(ages[fullAge.indexOf(true)]); //value that stored in the index;
 
-for (var i = 0; i < ES5Boxes.length; i++) {
-  if (ES5Boxes[i].className === "box blue") {
-    continue;
-  }
-  ES5Boxes[i].textContent = "YOU FOOL!";
-}
-
-//In ES6 we have For off loop which works like forEach loop but can use continue and break
-var Boxes = document.querySelectorAll(".box");
-var ES6Boxes = Array.from(Boxes); //Array.from is an ES6 method.
-
-for (const el of ES6Boxes) {
-  if (el.className.includes("blue")) {
-    //.includes is a new ES6 string method.
-    continue;
-  }
-  el.textContent = "YOU FOOL TOO!";
-}
+//in ES6 there are findIndex and find methods for it.
+var fullAgeIndex = ages.findIndex((el) => el >= 18);
+console.log(fullAgeIndex);
+var fullAge = ages.find((el) => el >= 18);
+console.log(fullAge);
