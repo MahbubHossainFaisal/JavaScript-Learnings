51b493efcb04499b9a1496a493fc4b4855ec9e8b
Mahbub Hossain Faisal <mahbubhossain249@gmail.com>
2020-08-21 21:58:54 +0600

    Map in ES6

--- ES6/script.js
+++ ES6/script.js
@@ -1,43 +1,41 @@
-//Spread Operator of ES6
-//Example 1:
-//in ES5
-var ages = [12, 14, 11, 16];
-function sum(a, b, c, d) {
-  return a + b + c + d;
+//Map in ES6
+
+var question = new Map();
+//set operation.
+question.set(
+  "question",
+  "What is the official name of major JavaScript version?"
+);
+question.set(1, "ES5");
+question.set(2, "ES6");
+question.set(3, "ES2015");
+question.set(4, "ES7");
+question.set("correct", 3);
+question.set(true, "Correct answer :D");
+question.set(false, "Wrong answer!Try again.");
+//get operation
+console.log(question.get("question"));
+console.log(question.get(3));
+console.log(question.get(false));
+if (question.has(false)); //has operation.
+console.log(question.get(4));
+
+//delete operator
+question.delete(4);
+console.log(question);
+
+//clear operator to delete all.
+//question.clear();
+
+//We can use for off loop to iterate through the map;
+
+for (let [key, value] of question.entries()) {
+  if (typeof key == "number") {
+    console.log(`Answer ${key} : ${value}`);
+  }
 }
 
-//if we want to pass array elements to that function we have to use apply method in ES5
+//another example
 
-var result = sum.apply(null, ages);
-console.log(result);
-
-//In ES6 we have spread operator  (...) to do that easily.
-
-var Ages = [12, 14, 11, 16];
-function Sum(a, b, c, d) {
-  return a + b + c + d;
-}
-
-var Result = Sum(...Ages);
-console.log(Result);
-
-//Example 2:
-
-//If we want to merge to arrays we can do that simply with spread operator
-
-var firstname = ["Mahbub", "Monwar", "Fatema"];
-var lastname = ["Hosna", "Shanta", "Chayna"];
-
-var FullName = [...firstname, ...lastname];
-console.log(FullName);
-//if we want to add more elements inside we can do that too.
-var Full = [...firstname, "Amena", ...lastname];
-console.log(Full);
-
-//Example 3:
-
-const h = document.querySelector("h1");
-const boxes = document.querySelectorAll(".box");
-const all = [h, ...boxes]; //mergin all query selectors with spread operator.
-
-Array.from(all).forEach((el) => (el.style.color = "navy blue")); //converting them in array and changing the color of text;
+var ans = parseInt(prompt("Write the correct answer:"));
+console.log(question.get(ans === question.get("correct"))); //ans===question.get("correct") will return true
